b621f1e186ff002f49df690470e7b6f548006c2e
deevyanshugarg <devyanshugarg2000@gmail.com>
2025-12-01 01:33:12 +0530

    Other files

---
+++ parsingResponse/parsingResponse/src/main/java/com/deevyanshu/parsingResponse/ParsingResponseApplication.java
@@ -0,0 +1,13 @@
+package com.deevyanshu.parsingResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingResponseApplication.class, args);
+	}
+
+}
