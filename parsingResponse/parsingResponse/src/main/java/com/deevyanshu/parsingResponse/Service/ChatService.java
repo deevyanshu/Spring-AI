41c7a943e7179f5fa7b1529b3118bbfbe0e4dbcb
deevyanshugarg <devyanshugarg2000@gmail.com>
2025-12-01 22:42:40 +0530

    Other files

--- parsingResponse/parsingResponse/src/main/java/com/deevyanshu/parsingResponse/Service/ChatService.java
+++ parsingResponse/parsingResponse/src/main/java/com/deevyanshu/parsingResponse/Service/ChatService.java
@@ -3,6 +3,7 @@ package com.deevyanshu.parsingResponse.Service;
 import com.deevyanshu.parsingResponse.Entity.Tut;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
@@ -42,6 +43,18 @@ public class ChatService {
     public List<Tut> chatList(String propt)
     {
         Prompt prompt1=new Prompt(propt);
+
+        List<Tut> tutorials=chatClient.prompt(prompt1).call().entity(new ParameterizedTypeReference<List<Tut>>() {
+        });
+
+        return tutorials;
+    }
+
+    //this is for prompt properties
+    public List<Tut> chatListPrompt(String prompt)
+    {
+        Prompt prompt1=new Prompt(prompt, GoogleGenAiChatOptions.builder().
+                model("gemini-2.5-flash").temperature(0.2).maxOutputTokens(1000).build());
 
         List<Tut> tutorials=chatClient.prompt(prompt1).call().entity(new ParameterizedTypeReference<List<Tut>>() {
         });
